1a651b2157a5ff95390ccf80f26e827143f9434d
mejerrytech <134769418+mejerrytech@users.noreply.github.com>
2024-03-17 21:53:36 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a sample frontend app.</p>
+    </div>
+  );
+}
+
+export default App;
